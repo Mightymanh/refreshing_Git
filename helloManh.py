75c7018aeaa2e60d687b0510a8eac1d4343dc175
Mightymanh <dphan8@uic.edu>
2023-01-07 09:42:36 -0600

    modify helloManh.py

--- helloManh.py
+++ helloManh.py
@@ -3,11 +3,13 @@ def helloManh(num_rep):
   This function prints out "hello Manh" num_rep times
   """
   try:
+    num_rep = int(num_rep)
     for i in range(num_rep):
       print("hello Manh")
   except:
     print("something is wrong with num_rep")
     print("but hello Manh :)")
+
     
-num_rep = int(input("enter a number: "))
+num_rep = input("enter a number: ")
 helloManh(num_rep)
